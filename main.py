ee1bb2a00692566487505b8ae501bc272cf4a2bb
ujincn <ujin.cn@gmail.com>
2019-05-06 13:27:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import logging
 import components.porn91 as porn91
 
 logging.basicConfig(level=10,
-                        format='%(asctime)s [%(module)s] %(levelname)-8s %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S')
+                    format='%(asctime)s [%(module)s] %(levelname)-8s %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
 
 if __name__ == '__main__':
     porn = porn91.Porn91()
